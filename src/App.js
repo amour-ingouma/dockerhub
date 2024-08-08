6f1ca8362a7b7185b03331ed4a102e6839552143
hubert <hubert@inexa-sys-pc2.inexa-ci.lan>
2024-08-08 19:05:02 +0200

    docker commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	<h1>Hello World</h1>
+	<p>Hubert est la</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
